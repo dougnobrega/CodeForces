54e6199d5a991b4a9060376a600ce34dfef4eb15
Doug Nóbrega <dougnobrega@alu.ufc.br>
2020-03-09 16:09:26 -0300

    Problem B - Biodiversity

---
+++ gym/SWERC2019-2020/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Prioridade
+typedef long long   ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < (int)(b); i++)
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define endl '\n'
+#define mdc(a, b) (__gcd((a), (b)))
+#define mmc(a, b) (((a)/__gcd(a, b)) * b)
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+// fim da Prioridade
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	map<string, int> count;
+	int best = 0;
+	
+	string s;
+	string ans;
+	for (int i = 0; i < n; i++){
+		string s;
+		cin >> s;
+		count[s]++;
+		if(best < count[s]){
+			best = count[s];
+			ans = s;
+		}
+	}
+	if(n < 2 * best){
+		cout << ans << endl; 
+	}else{
+		cout << "NONE" << endl;
+	}
+	
+	return 0;
+}
+
