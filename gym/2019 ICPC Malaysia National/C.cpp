c703bc6e8886b29f1d9be3486ce10ea18544c0b1
Doug Nóbrega <dougnobrega@alu.ufc.br>
2020-03-24 00:18:53 -0300

    Problem C - I Don't Want To Pay For The Late Jar!

---
+++ gym/2019 ICPC Malaysia National/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Prioridade
+typedef long long   ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < (int)(b); i++)
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define endl '\n'
+#define mdc(a, b) (__gcd((a), (b)))
+#define mmc(a, b) (((a)/__gcd(a, b)) * b)
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+// fim da Prioridade
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int d;
+	cin >> d;
+	for (int ca = 1; ca <= d; ca++){
+		int n, s;
+		cin >> n >> s;
+		int ans = -INF;
+		for (int i = 0; i < n; i++){
+			int f, t;
+			cin >> f >> t;
+			if(t <=s){
+				ans = max(ans, f);
+			}else{
+				ans = max(ans, f - (t - s));
+			}
+		}
+		cout << "Case #" << ca << ": " << ans << endl;
+		
+	}
+	
+	return 0;
+}
+	
+
