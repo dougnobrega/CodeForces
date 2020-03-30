aa424d9a56446372583509b04bba9ceaa637482e
Doug Nóbrega <dougnobrega@alu.ufc.br>
2020-03-30 15:12:18 -0300

    Problem D - Meeting Bahosain

---
+++ gym/ArabellaCPC 2019 /D.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Prioridade
+typedef long long   ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < (int)(b); i++)
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define endl '\n'
+#define mdc(a, b) (__gcd((a), (b)))
+#define mmc(a, b) (((a)/__gcd(a, b)) * b)
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+// fim da Prioridade
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	vi a(n);
+	vi b(k);
+	for (int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	ll gcd = 0;
+	for (int i = 0; i < k; i++){
+		cin >> b[i];
+		gcd = mdc(gcd,(ll)b[i]);
+	}
+	ll aux = a[0]%gcd, ok = 1;
+	for (int i = 0; i < n; i++){
+		if(a[i]%gcd != aux) ok = 0;
+	}
+	if(ok){
+		cout << "Yes" << endl;
+	}else{
+		cout << "No" << endl;
+	}	
+	
+	return 0;
+}
+
+
