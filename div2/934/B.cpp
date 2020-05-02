a819264c3c2b7b0ee9d6042e4b765d63b4e79932
Doug Nóbrega <dougnobrega@alu.ufc.br>
2020-05-02 10:18:23 -0300

    Problem B - A Prosperous Lot

---
+++ div2/934/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Prioridade
+typedef long long   ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < (int)(b); i++)
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007ll
+#define endl '\n'
+#define mdc(a, b) (__gcd((a), (b)))
+#define mmc(a, b) (((a)/__gcd(a, b)) * b)
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+// fim da Prioridade
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int k;
+	cin >> k;
+	string ans;
+	while(k != 0){
+		if(k >= 2){
+			ans += '8';
+			k-=2;
+		}else{
+			ans += '4';
+			k--;
+		}	
+	}
+	if(ans.size() > 18) cout << -1 << endl;
+	else cout << ans << endl;
+	return 0;
+}
+
