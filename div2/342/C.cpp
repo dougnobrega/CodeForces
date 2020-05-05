d131d326fab1e2e310f647e413f6789e1f5a2499
Doug Nóbrega <dougnobrega@alu.ufc.br>
2020-05-05 17:20:20 -0300

    Problem C - Cupboard and Ballons

---
+++ div2/342/C.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Prioridade
+typedef long long   ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < (int)(b); i++)
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define endl '\n'
+#define mdc(a, b) (__gcd((a), (b)))
+#define mmc(a, b) (((a)/__gcd(a, b)) * b)
+#define W(x) cerr << "\033[31m"<<  #x << " = " << x << "\033[0m" << endl;
+// fim da Prioridade
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int r, h;
+	cin >> r >> h;
+	ll ans = 0;
+	while(h >= r){
+		ans += 2;
+		h -= r;
+	}
+	
+	if (4LL * h * h >= 3LL * r * r)
+        ans += 3;
+    else if (2LL * h >= r)
+        ans += 2;
+    else
+        ans += 1;
+        
+	cout << ans << endl;
+	return 0;
+}
+
+
